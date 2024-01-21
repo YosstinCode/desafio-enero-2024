e51e7904ffbec4ae336f94646fd9749e13474139
YosstinCode <76113738+YosstinCode@users.noreply.github.com>
2024-01-21 17:38:52 -0500

    feat: implement word inversion method This commit addresses part 1 of the challenge, where a method was created to invert the words in a text string.

--- src/main/java/com/programandoenjava/parte1/Parte1.java
+++ src/main/java/com/programandoenjava/parte1/Parte1.java
@@ -1,15 +1,46 @@
 package com.programandoenjava.parte1;
 
+/**
+ *
+ */
+
 public class Parte1 {
 
     /**
-     * Escribe un programa en Java que reciba una cadena de texto y devuelva la
-     * misma cadena pero con cada palabra invertida. Por ejemplo, para la entrada "Hola Mundo",
-     * la salida debería ser "aloH odluM".
+     * Este método se encarga de invertir las palabras de una cadena sin considerar ningún otro símbolo. Por ejemplo:
+     *
+     *<ul>
+     *     <li>Si la cadena de entrada es "hola mundo", la cadena de salida será "aloh odnum".</li>
+     *     <li>Si la cadena de entrada es "¡hola mundo!", la cadena de salida será "¡aloh odnum!".</li>
+     *</ul>
+     *
      * @param cadena cadena de entrada
-     * @return cadena invertida
+     * @return cadena invertida sin considerar ningún otro símbolo o cadena vacía si la cadena de entrada es nula o vacía
+     *
+     * @Author: YosstinCode
+     * @Version: 1.0
+     *
      */
-    public String invertirPalabras(final String cadena) {;
-        return null;
+    public String invertirPalabras(final String cadena) {
+
+        if(cadena == null || cadena.isBlank()) return "";
+
+        StringBuilder stringInverse = new StringBuilder();
+
+        int positionForInsertedLetter = 0;
+
+        for (char letter : cadena.toCharArray()) {;
+
+            if(!Character.isLetter(letter)) {
+                stringInverse.append(letter);
+                positionForInsertedLetter = stringInverse.lastIndexOf(Character.toString(letter)) + 1;
+                continue;
+            }
+
+            stringInverse.insert(positionForInsertedLetter, letter);
+
+        }
+
+        return stringInverse.toString();
     }
 }
